853f29cc3e0cc87dc4aca1fa35ec47695a735e88
Xverlxrd <tur-2002@mail.ru>
2023-04-12 18:01:17 +0700

    devolop subnote

--- test-app-mcc/src/Components/noteItem.jsx
+++ test-app-mcc/src/Components/noteItem.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 function NoteItem(props) {
+  const [subNotes, setSubNotes] = useState([]);
+
+  const addSubNote = () => {
+    const newSubNote = {
+      id: Date.now(),
+      title: "Новая подзаметка",
+      subNotes: [], // Вложенные подзаметки
+    };
+    setSubNotes([...subNotes, newSubNote]);
+  };
+
   return (
-    <div className="note__item">
-      <input
-        className="note__item_checkbox"
-        type={"radio"}
-        checked={props.isChecked}
-        onChange={props.onNoteItemClick}
-      />
-      <h1 className="note__item_title">{props.note.title}</h1>
+    <div className="note__item_container">
+      <div className="note__item">
+        <input
+          className="note__item_checkbox"
+          type={"radio"}
+          checked={props.isChecked}
+          onChange={props.onNoteItemClick}
+        />
+        <h1 className="note__item_title">{props.note.title}</h1>
+        <button onClick={addSubNote}>Add</button>
+      </div>
+      <div className="subnote__container">
+      {subNotes.map((subNote) => (
+        <NoteItem key={subNote.id} note={subNote} />
+      ))}
+      </div>
     </div>
   );
 }
